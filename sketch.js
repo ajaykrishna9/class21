8e5ff39dc358f39de81a996e54830d0ca12f2986
ajaykrishna9 <74241478+ajaykrishna9@users.noreply.github.com>
2020-11-25 16:24:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 200, 50, 80);
+  
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.velocityX=2
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0,0,0);  
+ // movingRect.x = World.mouseX;
+ // movingRect.y = World.mouseY;
+bounceoff(movingRect,fixedRect);
+ 
+  drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
+
